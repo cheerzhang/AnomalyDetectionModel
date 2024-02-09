5c9d5ddb5a899f775d57b2327b9a56660304d709
cheerzhang <zhanglenus@gmail.com>
2024-02-09 13:33:13 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,8 +11,8 @@ project = 'quick-anomaly-detector'
 copyright = '2024, LeZhang'
 author = 'LeZhang'
 
-release = '0.1'
-version = '0.1.1'
+release = '0.2'
+version = '0.2.1'
 
 # -- General configuration
 
