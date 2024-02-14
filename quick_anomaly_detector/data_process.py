14969fec4842ffa93101c93ff13f355caeb69b8f
cheerzhang <zhanglenus@gmail.com>
2024-02-14 10:43:33 +0100

    remove location data

--- quick_anomaly_detector/data_process.py
+++ quick_anomaly_detector/data_process.py
@@ -283,24 +283,3 @@ def calculate_metrics(actual_labels, predicted_labels):
     return metrics
 
 
-
-#########################################
-#              Location API             #
-#########################################
-from .json_file import address_json
-def location_json(city='', street=''):
-    lat, lon = [None, None]
-    if city != '':
-        if city in address_json.keys():
-            lat, lon = address_json[city]['lat'], address_json[city]['lon']
-        else:
-            lat = 91
-            lon = 181
-    return lat, lon
-def get_location(city):
-    lat, lon = location_json(city=city)
-    return lat, lon
-def get_lat_lon(df, city_column):
-    """This function is get lat and long of the city column's value"""
-    df['lat'], df['lon'] = zip(*df[city_column].apply(get_location))
-    return df['lat'].values, df['lon'].values
