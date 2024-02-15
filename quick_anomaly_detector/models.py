f79138c13c572211a6dcd277a5673c3ffdf10449
cheerzhang <zhanglenus@gmail.com>
2024-02-15 14:01:45 +0100

    add voc dict

--- quick_anomaly_detector/models.py
+++ quick_anomaly_detector/models.py
@@ -566,7 +566,7 @@ class TrainEmbedding:
         self.model = None
         self.optimizer = None
         self.criterion = None
-        self.letter_to_number = {'a': 1,  'b': 2,  'c': 3,  'd': 4,  'e': 5,  'f': 6,  'g': 7,  'h': 8,  'i': 9,  'j': 10, 
+        self.letter_to_number = {'a': 1,  'b': 2,  'c': 3,  'd': 4,  'e': 5,  'f': 6,  'g': 7,  'h': 8,  'i': 9,  'j': 10,
             'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 
             'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26, 
             'A': 27, 'B': 28, 'C': 29, 'D': 30, 'E': 31, 'F': 32, 'G': 33, 'H': 34, 'I': 35, 'J': 36, 
@@ -574,7 +574,7 @@ class TrainEmbedding:
             'U': 47, 'V': 48, 'W': 49, 'X': 50, 'Y': 51, 'Z': 52, 
             '0': 53, '1': 54, '2': 55, '3': 56, '4': 57, '5': 58, '6': 59, '7': 60, '8': 61, '9': 62,
             '.': 63, '-': 64, ' ': 65, '@': 66, '?': 67, '/': 68,  "'": 69, "|": 70, 
-            '+': 71, }
+            '+': 71, 'â': 72}
         self.max_length = 0
         self.batch_size = batch_size
         self.vocab_size = len(self.letter_to_number)+1
