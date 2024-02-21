9296ddbb7da30db7b5d574f40d9466f86b8a13d9
cheerzhang <zhanglenus@gmail.com>
2024-02-21 10:01:53 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='quick-anomaly-detector',
-    version='0.2.15',
+    version='0.2.16',
     author="ZhangLe",
     author_email="zhangle@gmail.com",
     description="models class for quick Anomaly Detection",
